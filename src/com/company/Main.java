b7d9a7f27a4afe6fe968802b058926e1f55c8083
Wi1iku <oscarchudy984@gmail.com>
2022-03-02 13:43:50 +0100

    server peta cuando se sale alguien, añadido comando de desconexion

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,84 @@
 package com.company;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
+        BufferedWriter bufferEscritura = null;
+        {
+            try {
+                bufferEscritura = new BufferedWriter(new FileWriter("archivolog",true));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        assert bufferEscritura != null;
+        PrintWriter logger = new PrintWriter(bufferEscritura);
+
         try {
             serverSocket = new ServerSocket(1050);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Servidor inicializado");
+
+        logger.println("Servidor inicializado "+LocalDateTime.now().format(DateTimeFormatter.ofPattern("'[Dia]' dd/MM/yyyy '[Hora del dia]' hh:mm:ss.SSS")));
+        logger.flush();
+
+
+        BufferedWriter finalBufferEscritura = bufferEscritura;
+        Runtime.getRuntime().addShutdownHook(new Thread()
+                                             {
+        @Override
+                                                 public void run(){
+            logger.println("Servidor cerrado "+LocalDateTime.now().format(DateTimeFormatter.ofPattern("'[Dia]' dd/MM/yyyy '[Hora del dia]' hh:mm:ss.SSS")));
+            logger.flush();
+            logger.close();
+            //Este close no tiene mucho sentido, no se puede hacer close de otra manera
+            try {
+                finalBufferEscritura.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+                                             }
+
+
+        );
         while (true){
             assert serverSocket != null;
-            if (!!serverSocket.isClosed()) break;
-            Socket socket = null;
+            if (serverSocket.isClosed()) break;
+            Socket socket;
             try {
-                System.out.println("test");
                 socket = serverSocket.accept();
                 if (socket!=null){
                     System.out.println(socket);
-                    ManejadorCliente manejadorCliente = new ManejadorCliente(socket);
+                    ManejadorCliente manejadorCliente = new ManejadorCliente(socket,logger);
                     manejadorCliente.start();
                 }
-                System.out.println("test2");
+
             } catch (IOException e) {
                 e.printStackTrace();
 
             }
         }
+        //logger.println(manejadorCliente.getLogg());
+
+
     }
+
 }
--- src/com/company/ManejadorCliente.java
+++ src/com/company/ManejadorCliente.java
@@ -1,19 +1,32 @@
 package com.company;
 
 import java.io.*;
+import java.net.Inet4Address;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 public class ManejadorCliente extends Thread {
     public static ArrayList<ManejadorCliente> manejadorClientes= new ArrayList<>();
     int identificador=0;
-    private Socket socket;
+    private final Socket socket;
     private String nombre;
+    String salida;
+    String entrada;
+    PrintWriter logger;
+    String ip;
+    String logg;
+
+    long totalmensajessesion=-1;
+
+
     private BufferedReader bufferLeer;
     private BufferedWriter bufferEscribir;
     boolean salido=false;
-    public ManejadorCliente(Socket socket) {
+    public ManejadorCliente(Socket socket,PrintWriter logger) {
         this.socket=socket;
+        this.logger = logger;
         try {
             this.bufferLeer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.bufferEscribir = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
@@ -26,24 +39,41 @@ public class ManejadorCliente extends Thread {
             cerrartodo();
         }
         manejadorClientes.add(identificador,this);
-        System.out.println("usuario añadido");
+        System.out.println("svsided:"+nombre+" conectado al servidor");
         broadcast("Servidor: "+nombre+" se ha unido al chat de grupo");
         identificador++;
     }
 
     @Override
     public void run() {
+        try{
+        Inet4Address inet4 = (Inet4Address)socket.getInetAddress();
+        ip=inet4.toString();
+        }catch (Exception e){
+            ip="Error al guardar ip";
+        }
+        entrada = LocalDateTime.now().format(DateTimeFormatter.ofPattern("'[Dia]' dd/MM/yyyy '[Hora del dia]' hh:mm:ss.SSS"));
         String muchotexto =" ";
-        while (socket.isConnected()){
+        while (!socket.isClosed() && bufferLeer!=null){
             try {
                 if(!salido) {
+
                     muchotexto = bufferLeer.readLine();
+                    totalmensajessesion++;
                 }
-                while (!salido){
-                broadcast(muchotexto);}
+                if (!salido){
+                    if (muchotexto.equals("%\"Ju6A9jI2js\"%")){
+                        //salida = LocalDateTime.now().format(DateTimeFormatter.ofPattern("'Dia ' dd/MM/yyyy 'Hora del dia' hh:mm:ss.SSS"));
+                        this.cerrartodo();
+                        salido=true;
+
+
+                    }else{
+                    System.out.println("cl: "+muchotexto);
+                broadcast(muchotexto);}}
             } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("error en conexion");
+
+                System.out.println("error en conexion de "+nombre);
                 cerrartodo();
             }
         }
@@ -63,9 +93,15 @@ public class ManejadorCliente extends Thread {
             }
         }
     }
+
     private void cerrartodo(){
         try {
+
+            salida = LocalDateTime.now().format(DateTimeFormatter.ofPattern("'[Dia]' dd/MM/yyyy '[Hora del dia]' hh:mm:ss.SSS"));
+            logg ="Usuario: "+nombre+" /conexion: "+entrada+" /desconexion: "+salida+" /total mensajes: "+totalmensajessesion+ "/IP usuario: "+ip+ " /";
             salido=true;
+            logger.println(logg);
+            logger.flush();
             if(socket!=null){
                 socket.close();
             }
